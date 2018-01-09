eb285690cd6340a50bee326f02ac22039770800d
Christopher Svec <csvec@irobot.com>
2018-01-08 22:23:04 -0500

    Through lesson 13.

--- feature_extraction.py
+++ feature_extraction.py
@@ -19,7 +19,11 @@ fc7 = AlexNet(resized, feature_extract=True)
 # HINT: Look at the final layer definition in alexnet.py to get an idea of what this
 # should look like.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-probs = ...
+sigma = 0.01
+new_fc8_weights = tf.Variable(tf.truncated_normal(shape, stddev=sigma))
+new_fc8_bias    = tf.Variable(tf.zeros(nb_classes))
+logits = tf.matmul(fc7, new_fc8_weights) + new_fc8_bias
+probs = tf.nn.softmax(logits)
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
